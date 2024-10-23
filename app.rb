7df11519fd8f4a4c09f00cc44e3a7b1158c537ee
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-23 20:34:21 +0000

    Started gets

--- app.rb
+++ app.rb
@@ -7,3 +7,22 @@ end
 
 get("/images/rps.jpg") do
 end
+
+get("/rock") do
+  choices = ["rock","paper","scissors"]
+  play = "rock"
+  counter = choices.sample
+end
+
+get("/paper") do
+  choices = ["rock","paper","scissors"]
+  play = "paper"
+  counter = choices.sample
+end
+
+get("/scissors") do
+  choices = ["rock","paper","scissors"]
+  play = "scissors"
+  counter = choices.sample
+
+end
