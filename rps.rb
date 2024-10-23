e12be418aedc9f92e764457957523658f3345988
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-23 20:25:01 +0000

    Initial updates; finished text for welcome page but images are broken for some reason

---
+++ rps.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require "sinatra/reloader" # enables automatic reloading of code
+
+# Better error messages
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  erb(:welcome)
+end
