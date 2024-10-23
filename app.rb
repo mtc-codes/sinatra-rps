366a3f77260634cd419d38bbca4bdc44e2fc71e5
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-23 20:53:43 +0000

    Fixed play pages

--- app.rb
+++ app.rb
@@ -12,17 +12,41 @@ get("/rock") do
   choices = ["rock","paper","scissors"]
   play = "rock"
   counter = choices.sample
+
+  @my_play = "We played #{play}!"
+  @counter_play = "They played #{counter}!"
+
+  erb(:rock)
+  
 end
 
 get("/paper") do
   choices = ["rock","paper","scissors"]
   play = "paper"
   counter = choices.sample
+
+  @my_play = "We played #{play}!"
+  @counter_play = "They played #{counter}!"
+
+  erb(:paper)
+  
 end
 
 get("/scissors") do
   choices = ["rock","paper","scissors"]
   play = "scissors"
   counter = choices.sample
+
+  @my_play = "We played #{play}!"
+  @counter_play = "They played #{counter}!"
+  @outcome = 
+    if counter == play
+      "We tied!"
+    elsif counter = "rock"
+      "We lost!"
+    else 
+      "we won!"
+    end
+  erb(:scissors)
 
 end
